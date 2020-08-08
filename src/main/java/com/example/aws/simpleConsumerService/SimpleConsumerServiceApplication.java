d5c879d47ccf41745d95dd02e622ae0307e911dc
Carl Saptarshi <carlsaptarshi@rentalcars.com>
2020-08-09 00:14:24 +0100

    first commit

---
+++ src/main/java/com/example/aws/simpleConsumerService/SimpleConsumerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.aws.simpleConsumerService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleConsumerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleConsumerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aws/simpleConsumerService/SimpleConsumerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aws.simpleConsumerService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleConsumerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
